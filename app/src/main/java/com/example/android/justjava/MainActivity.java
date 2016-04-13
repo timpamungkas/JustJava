f159ff2e5b463d4766df7b5a205ee66fd78c5b60
timpamungkas <timotius.pamungkas@gmail.com>
2016-04-13 21:18:43 +0700

    ok

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.android.justjava;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import java.text.NumberFormat;
@@ -13,6 +15,9 @@ import java.text.NumberFormat;
 public class MainActivity extends ActionBarActivity {
 
     private int quantity;
+    private final String LS = System.getProperty("line.separator");
+    private boolean isWhippedCream = true;
+    private boolean isChocolateCream = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,35 +25,59 @@ public class MainActivity extends ActionBarActivity {
         setContentView(R.layout.activity_main);
     }
 
+    private String createOrderSummary(int cups) {
+        StringBuilder sb = new StringBuilder();
+        CheckBox chkWhippedCream = (CheckBox) findViewById(R.id.is_whipped_cream);
+        CheckBox chkChocolateCream = (CheckBox) findViewById(R.id.is_chocolate_cream);
+        EditText txtName = (EditText) findViewById(R.id.txt_name);
+
+        this.isWhippedCream = chkWhippedCream.isChecked();
+        this.isChocolateCream = chkChocolateCream.isChecked();
+
+        sb.append("Name : " + txtName);
+        sb.append(LS);
+        sb.append("Use whipped cream : " + isWhippedCream);
+        sb.append(LS);
+        sb.append("Use chocolate cream : " + isChocolateCream);
+        sb.append(LS);
+        sb.append("Quantity : " + cups);
+        sb.append(LS);
+        sb.append("Total : " + NumberFormat.getCurrencyInstance().format(calculatePrice(cups)));
+        sb.append(LS);
+        sb.append("Thank You");
+
+        return sb.toString();
+    }
+
     /**
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-//        display(quantity);
-//        displayPrice(quantity * 5);
-        displayMessage("Thank you for order");
+        String summary = createOrderSummary(this.quantity);
+        displayMessage(summary);
     }
 
     private void displayMessage(String message) {
-        TextView messageTextView = (TextView) findViewById(R.id.message_text_view);
+        TextView messageTextView = (TextView) findViewById(R.id.order_summary_text_view);
         messageTextView.setText(message);
     }
 
     public void increment(View view) {
         this.quantity++;
-        submitOrder(view);
+
+        display(this.quantity);
     }
 
     public void decrement(View view) {
         if (this.quantity > 0) {
             this.quantity--;
         }
-        submitOrder(view);
+
+        display(this.quantity);
     }
 
-    private void displayPrice(int number) {
-        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
-        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
+    private int calculatePrice(int cups) {
+        return cups * 5;
     }
 
     /**
